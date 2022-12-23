ca2dc56a96ceab05e2e4c65582b246340eb03215
haylove <hfayelovelady@gmail.com>
2022-12-23 14:44:03 -0600

    comments added to category model

--- models/Category.js
+++ models/Category.js
@@ -2,15 +2,24 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+//create new sequelize model for category
 class Category extends Model {}
 
+//initialize category model
 Category.init(
   {
     // define columns
+    id: {
+
+    }
   },
+
+  //second argument
   {
+    //link to database connection (utilizing sequelize)
     sequelize,
     timestamps: false,
+    //enforcing that model name is same as table name
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
