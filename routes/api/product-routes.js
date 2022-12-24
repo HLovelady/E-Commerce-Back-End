9ccc0ab65aeadbd6128cb6463c1a2da1d3e85f93
haylove <hfayelovelady@gmail.com>
2022-12-24 15:56:50 -0600

    findAll products in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,27 +6,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+  //associated Category and Tag data
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+
+//NEED TO ADD THIS
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
+
+
 // create new product
 router.post('/', (req, res) => {
+  Product.create({req.body
+    // NEED TO ADD THIS
   /* req.body should look like this...
-    {
+
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
-    }
   */
-  Product.create(req.body)
-    .then((product) => {
+  })
+    
+  
+  
+  .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -47,6 +73,9 @@ router.post('/', (req, res) => {
     });
 });
 
+
+
+// UPDATE COMPLETE
 // update product
 router.put('/:id', (req, res) => {
   // update product data
@@ -89,6 +118,10 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
+
+
+// NEED TO DO DELETE
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
 });
